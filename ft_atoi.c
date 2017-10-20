1b6afd57f968a1e26f1cdb4828fc836d13b30b66
MJHd <thedesignlabproject@gmail.com>
2017-10-20 03:13:17 -0700

    here we go

--- ft_atoi.c
+++ ft_atoi.c
@@ -12,18 +12,21 @@
 
 #include "libft.h"
 
-int	ft_atoi(const char *s)
+int		ft_atoi(const char *s)
 {
 	int value;
 	int multiplier;
 	int length;
 
 	value = 0;
-	multiplier = 1;
+	while(*s && (*s == ' ' || *s == '\n' || *s == '\f' || *s == '\t' || *s == '\v' || *s == '\r'))
+		s++;
+	s += (*s == '+') ? 1 : 0;
+	multiplier = ((*s) == '-' && *s++) ? -1 : 1;
 	length = (int)ft_strlen(s);
-	while (length)
+	while (length && s[--length])
 	{
-		value += ((s[--length] - '0') * multiplier);
+		value += ((s[length] - '0') * multiplier);
 		multiplier *= 10;
 	}
 	return(value);
