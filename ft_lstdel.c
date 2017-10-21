465b45352d3de505aa9fd31b0f7d7814cdf41c00
MJHd <thedesignlabproject@gmail.com>
2017-10-21 16:59:00 -0700

    quick check3

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -16,10 +16,10 @@ void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
 	if (alst && (*alst))
 	{
-		if ((*(*alst)).next != NULL)
+		if ((*(*alst)).next != 0)
 			ft_lstdel(&(*(*alst)).next, del);
 		del((*(*alst)).content, (*(*alst)).content_size);
 		free(*alst);
-		*alst = NULL;
+		*alst = 0;
 	}
 }
