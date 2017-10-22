383279772be136ecb51024d2986e2dae3afd8ec8
MJHd <thedesignlabproject@gmail.com>
2017-10-21 17:06:29 -0700

    oh shit

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -16,11 +16,8 @@ void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
 	if (alst && (*alst))
 	{
-		if ((*(*alst)).next != 0)
-			ft_lstdel(&(*(*alst)).next, del);
-		del((*(*alst)).content, (*(*alst)).content_size);
-		free(*alst);
-		*alst = 0;
+		while ((*(*alst)).next != 0)
+			ft_lstdel(&(*(*alst++)).next, del);
 	}
 }
 //
