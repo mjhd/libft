544d58c966d6f23825d835cdaf64ad68eff6ed07
MJHd <thedesignlabproject@gmail.com>
2017-10-21 17:30:25 -0700

    oh shit B

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -14,9 +14,9 @@
 
 void	ft_lstiter(t_list *lst, void (*f)(t_list *elem))
 {
-	if (!lst)
-		return ;
-	if (lst->next)
-		ft_lstiter(lst->next, f);
-	f(lst);
+	while ((*lst).next)
+	{
+		f(lst);
+		lst = (*lst).next;
+	}
 }
