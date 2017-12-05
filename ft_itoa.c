5b3c2e3b980fe1c82009f2670c9d3bf421673017
mjhdcode <mjhd42code@gmail.com>
2017-12-04 20:00:10 -0800

    fuck ints

--- ft_itoa.c
+++ ft_itoa.c
@@ -12,36 +12,50 @@
 
 #include "libft.h"
 
+int itoa_assembler(struct inty *pnt, int n, struct inty **mod)
+{
+    struct inty *tmp;
+    int nodes;
+    int buf;
+
+    nodes = 0;
+    while((n || (!n && !nodes)) && ++nodes)
+    {
+        buf = n % 10;
+        n -= buf;
+        n /= 10;
+        (*pnt).value = (char)(buf + '0');
+        if(n)
+        {
+            tmp = (struct inty *)malloc(sizeof(struct inty));
+            (*tmp).last = pnt;
+        }
+        (*pnt).last = (nodes == 1) ? 0 : (*pnt).last;
+        pnt = tmp;
+    }
+    (*mod) = (nodes == 1) ? (*mod) : tmp;
+    return(nodes);
+}
+
 char        *ft_itoa(int n)
 {
-	struct inty *pnt;
-	struct inty *tmp;
-	char *a;
-	int buf;
-	int nodes;
+    struct inty *pnt;
+    struct inty **mod;
+    char *a;
+    int nodes;
+    int offset;
 
-	pnt = (struct inty *)malloc(sizeof(struct inty));
-	nodes = 0;
-	while(n && ++nodes)
-	{
-		buf = n % 10;
-		n -= buf;
-		n /= 10;
-		(*pnt).value = (char)(buf + '0');
-		if(n)
-		{
-			tmp = (struct inty *)malloc(sizeof(struct inty));
-			(*tmp).last = pnt;
-			(*pnt).last = (nodes == 1) ? 0 : (*pnt).last;
-			pnt = tmp;
-		}
-	}
-	a = malloc(sizeof(char) * (nodes + 1));
-	a[nodes] = '\0';
-	while(pnt)
-	{
-		*a++ = (*pnt).value;
-		pnt = (*pnt).last;
-	}
-	return(a - nodes);
+    pnt = (struct inty *)malloc(sizeof(struct inty));
+    mod = &pnt;
+    nodes = itoa_assembler(pnt, n, mod);
+    a = malloc(sizeof(char) * (nodes + 1));
+    a[nodes] = '\0';
+    offset = nodes;
+    while(nodes--)
+    {
+        *a++ = (*pnt).value;
+        pnt = (*pnt).last;
+    }
+    return(a - offset);
 }
+//
