4f09ecb26062b4cfac7cd6ae92a4a6dba2d65e49
mjhdcode <mjhd42code@gmail.com>
2017-12-08 15:36:05 -0800

    null check

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -12,23 +12,21 @@
 
 #include "libft.h"
 
-void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
+void    *ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
-	size_t			i;
-	const char		*cpy_src;
-	unsigned char	*cpy_dest;
-	unsigned char	cpy_c;
+    void *returner;
+    size_t i = 0;
 
-	i = 0;
-	cpy_c = (unsigned char)c;
-	cpy_src = src;
-	cpy_dest = dest;
-	while (i < n)
-	{
-		cpy_dest[i] = cpy_src[i];
-		if (cpy_dest[i] == cpy_c)
-			return (&cpy_dest[i + 1]);
-		i++;
-	}
-	return (NULL);
+    if (n <= 0)
+        return(0);
+    returner = (unsigned char*)dest;
+    while (i < n)
+    {
+        *(unsigned char*)dest++ = *(unsigned char*)src++;
+        if ((char)*((unsigned char*)dest - 1) == c)
+            return(returner);
+        i++;
+    }
+    return(NULL);
 }
+//
