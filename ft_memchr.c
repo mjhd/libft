bc0c225a080f0bd7de67fb02318590ec90bdbb3f
mjhdcode <mjhd42code@gmail.com>
2017-12-10 13:40:51 -0800

    merr

--- ft_memchr.c
+++ ft_memchr.c
@@ -14,7 +14,10 @@
 
 void    *ft_memchr(const void *s, int c, size_t n)
 {
-  while(n--)
+    size_t i;
+
+    i = 0;
+  while(i < n)
   {
     if(*(unsigned char *)s++ == (unsigned char)c)
       return(--s);
