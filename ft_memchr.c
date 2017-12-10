201a927e414d542abed4f26367895d3f5bd613c2
mjhdcode <mjhd42code@gmail.com>
2017-12-10 13:14:06 -0800

    meh

--- ft_memchr.c
+++ ft_memchr.c
@@ -12,20 +12,13 @@
 
 #include "libft.h"
 
-void	*ft_memchr(const void *s, int c, size_t n)
+void    *ft_memchr(const void *s, int c, size_t n)
 {
-	size_t			i;
-	unsigned char	cpy_c;
-	unsigned char	*cpy_src;
-
-	i = 0;
-	cpy_c = (unsigned char)c;
-	cpy_src = (unsigned char *)s;
-	while (i < n)
-	{
-		if (cpy_src[i] == cpy_c)
-			return (&cpy_src[i]);
-		i++;
-	}
-	return (NULL);
+  while(n--)
+  {
+    if(*(char *)s++ == c)
+      return(--s);
+  }
+  return(0);
 }
+//
