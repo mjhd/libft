86ad4efc3cd5454b75ca3bc8acce351748587302
mjhdcode <mjhd42code@gmail.com>
2017-12-11 18:28:14 -0800

    fluuuuufffffffffffffffffff

--- ft_memchr.c
+++ ft_memchr.c
@@ -14,18 +14,15 @@
 
 void    *ft_memchr(const void *s, int c, size_t n)
 {
-    size_t            i;
     unsigned char    cpy_c;
     unsigned char    *cpy_src;
 
-    i = 0;
     cpy_c = (unsigned char)c;
     cpy_src = (unsigned char *)s;
     while (n--)
     {
-        if (cpy_src[i] == cpy_c)
-            return (&cpy_src[i]);
-        i++;
+        if (*cpy_src++ == cpy_c)
+            return (cpy_src);
     }
     return (0);
 }
