f1df718ab686567faf432527703ac306afa47902
mjhdcode <mjhd42code@gmail.com>
2017-12-13 15:10:05 -0800

    memset uno

--- ft_memset.c
+++ ft_memset.c
@@ -12,17 +12,13 @@
 
 #include "libft.h"
 
-void	*ft_memset(void *s, int c, size_t n)
+void    *ft_memset(void *s, int c, size_t n)
 {
-	size_t	i;
-	char	*cpy;
+	void  *pnt;
 
-	i = 0;
-	cpy = s;
-	while (i < n)
-	{
-		cpy[i] = c;
-		i++;
-	}
-	return (cpy);
+	pnt = (void *)s;
+	while (n--)
+		*(unsigned char *)s++ = (unsigned char)c;
+	return (pnt);
 }
+//
