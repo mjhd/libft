1a14e98d4089c3701cfea5095b9882730907c7a9
mjhdcode <mjhd42code@gmail.com>
2017-12-13 15:58:49 -0800

    putstr fd

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -14,14 +14,7 @@
 
 void	ft_putstr_fd(char const *s, int fd)
 {
-	size_t	i;
-
-	if (s == NULL)
-		return ;
-	i = 0;
-	while (s[i])
-	{
-		ft_putchar_fd(s[i], fd);
-		i++;
-	}
+	while (s && *s)
+		ft_putchar_fd(*s++, fd);
 }
+//
