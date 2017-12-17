8178300415a4ec51ea8c22e094711d5dd100ef75
mjhdcode <mjhd42code@gmail.com>
2017-12-17 05:07:50 -0800

    big pusher

--- libft.h
+++ libft.h
@@ -87,7 +87,6 @@ char				*ft_strcat(char *s1, const char *s2);
 char				*ft_strchr(const char *s, int c);
 void				ft_strclr(char *s);
 int					ft_strcmp(const char *s1, const char *s2);
-int					ft_strcount(char const *s, char c);
 char				*ft_strcpy(char *dst, const char *src);
 void				ft_strdel(char **as);
 char				*ft_strdup(const char *s1);
