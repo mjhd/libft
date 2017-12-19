34eb2904891e05a88bdd004b3331eb3e05ece2ad
mjhdcode <mjhd42code@gmail.com>
2017-12-18 17:05:25 -0800

    strnequ? shittest

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -17,7 +17,7 @@ int            ft_strnequ(char const *s1, char const *s2, size_t size)
 	int response;
 
 	response = 0;
-	while((s1 && s2) && size-- && !response && (*s1 || *s2))
+	while((s1 && s2) && --size && !response && (*s1 || *s2))
 		response = (*s1) ? (*s1++ - *s2++) : (*s2++ - *s1++);
 	return((!response) ? 1 : 0);
 }
